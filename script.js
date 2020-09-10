480ddccbc8c285f8a26d44eeb0e424b31720bf52
NicolaiHJørgensen <nicoj98@gmail.com>
2020-09-10 20:15:36 +0200

    harmonies working

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 "use strict";
-
+document.addEventListener("DOMContentLoaded", randomColor);
 document.querySelector("input").addEventListener("input", getHexColor);
-const displayHexValue = document.querySelector(".color-container:nth-child(3) p");
-const displayRGBValue = document.querySelector(".color-container:nth-child(3) p + p");
-const displayHSLValue = document.querySelector(".color-container:nth-child(3) p + p + p");
+
+document.querySelectorAll("#harmony-selector input").forEach((button) => {
+  button.addEventListener("click", getSelectedHarmony);
+});
+
+let selectedHarmony = "analogous";
+let globalBaseHSL;
+
+function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
+
+  const hexColor = rgbToHex({ r, g, b });
+  showSelectedColor(hexColor);
+}
 
 function getHexColor() {
   const selectedHexColor = this.value;
@@ -19,11 +32,7 @@ function showSelectedColor(hexColor) {
   console.log("rgb", rgb);
   console.log("hsl", hsl);
   console.log("css-rgb", cssRGB);
-  console.log("\n");
-
-  showRGB(rgb, cssRGB);
-  showHex(hexColor);
-  showHSL(hsl);
+  showSelectedHarmony(hsl);
 }
 
 function hexToRGB(hexColor) {
@@ -73,9 +82,9 @@ function rgbToHsl(rgb) {
   s *= 100;
   l *= 100;
 
-  h = h.toFixed();
-  s = s.toFixed();
-  l = l.toFixed();
+  h = Number(h.toFixed());
+  s = Number(s.toFixed());
+  l = Number(l.toFixed());
 
   return { h, s, l };
 }
@@ -85,15 +94,204 @@ function rgbToCSS(rgb) {
   return cssValue;
 }
 
-function showHex(hexColor) {
-  displayHexValue.textContent = `hex: ${hexColor}`;
+function showSelectedHarmony(baseHSL) {
+  globalBaseHSL = baseHSL;
+  const harmonyArray = [
+    { h: baseHSL.h, s: baseHSL.s, l: baseHSL.l },
+    { h: baseHSL.h, s: baseHSL.s, l: baseHSL.l },
+    baseHSL,
+    { h: baseHSL.h, s: baseHSL.s, l: baseHSL.l },
+    { h: baseHSL.h, s: baseHSL.s, l: baseHSL.l },
+  ];
+
+  const calcHarmonyArray = calcSelectedHarmony(harmonyArray);
+  showColors(calcHarmonyArray);
+
+  console.log(calcHarmonyArray);
+  console.log("\n");
 }
 
-function showRGB(rgb, cssRGB) {
-  displayRGBValue.textContent = `rgb (${rgb.red}, ${rgb.green}, ${rgb.blue})`;
-  document.querySelector(".color3").style.backgroundColor = cssRGB;
+function getSelectedHarmony() {
+  selectedHarmony = this.value;
+  console.log(selectedHarmony);
+  updateHarmony();
 }
 
-function showHSL(hsl) {
-  displayHSLValue.textContent = `hsl (${hsl.h}, ${hsl.s}, ${hsl.l})`;
+function updateHarmony() {
+  console.log(globalBaseHSL);
+  showSelectedHarmony(globalBaseHSL);
+}
+
+function calcSelectedHarmony(harmonyArray) {
+  const baseHSL = harmonyArray[2];
+  if (selectedHarmony === "analogous") {
+    calcAnalogous(harmonyArray, baseHSL);
+  } else if (selectedHarmony === "monochromatic") {
+    calcMonochromatic(harmonyArray, baseHSL);
+  } else if (selectedHarmony === "triad") {
+    calcTriad(harmonyArray, baseHSL);
+  } else if (selectedHarmony === "complementary") {
+    calcComplementary(harmonyArray, baseHSL);
+  } else if (selectedHarmony === "compound") {
+    calcCompound(harmonyArray, baseHSL);
+  } else if (selectedHarmony === "shades") {
+    calcShades(harmonyArray, baseHSL);
+  }
+  return harmonyArray;
+}
+
+function calcAnalogous(harmonyArray, baseHSL) {
+  harmonyArray[0].h = baseHSL.h - 50;
+  harmonyArray[1].h = baseHSL.h - 25;
+  harmonyArray[3].h = baseHSL.h + 25;
+  harmonyArray[4].h = baseHSL.h + 50;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function calcMonochromatic(harmonyArray, baseHSL) {
+  harmonyArray[0].s = baseHSL.s - 30;
+  harmonyArray[1].s = baseHSL.s - 15;
+  harmonyArray[3].s = baseHSL.s + 15;
+  harmonyArray[4].s = baseHSL.s + 30;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function calcTriad(harmonyArray, baseHSL) {
+  harmonyArray[0].h = baseHSL.h - 60;
+  harmonyArray[1].h = baseHSL.h - 60;
+  harmonyArray[3].h = baseHSL.h + 60;
+  harmonyArray[4].h = baseHSL.h + 60;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function calcComplementary(harmonyArray, baseHSL) {
+  harmonyArray[0].h = baseHSL.h + 180;
+  harmonyArray[1].h = baseHSL.h + 180;
+  harmonyArray[3].h = baseHSL.h + 180;
+  harmonyArray[4].h = baseHSL.h + 180;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function calcCompound(harmonyArray, baseHSL) {
+  harmonyArray[0].h = baseHSL.h - 50;
+  harmonyArray[1].h = baseHSL.h + 180;
+  harmonyArray[3].h = baseHSL.h + 180;
+  harmonyArray[4].h = baseHSL.h + 50;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function calcShades(harmonyArray, baseHSL) {
+  harmonyArray[0].l = baseHSL.l - 35;
+  harmonyArray[1].l = baseHSL.l - 15;
+  harmonyArray[3].l = baseHSL.l + 15;
+  harmonyArray[4].l = baseHSL.l + 35;
+  validateHarmony(harmonyArray);
+  return harmonyArray;
+}
+
+function validateHarmony(harmonyArray) {
+  harmonyArray.forEach((hsl) => {
+    if (hsl.h < 0) {
+      hsl.h += 360;
+    } else if (hsl.l > 360) {
+      hsl.h -= 360;
+    }
+
+    if (hsl.s < 0) {
+      hsl.s -= 0 + hsl.s;
+    } else if (hsl.s > 100) {
+      hsl.s -= hsl.s - 100;
+    }
+
+    if (hsl.l < 0) {
+      hsl.l -= 100;
+    } else if (hsl.l > 100) {
+      hsl.l += 100;
+    }
+  });
+
+  // TODO: check if h is between 0 and 360 and if s,l is between 0 and 100
+}
+
+function showColors(calcHarmonyArray) {
+  calcHarmonyArray.forEach((hslObject) => {
+    showColor(calcHarmonyArray, hslObject);
+  });
+}
+
+function showColor(calcHarmonyArray, hsl) {
+  const rgb = hslToRGB(hsl);
+  const hex = rgbToHex(rgb);
+  displayHarmony(calcHarmonyArray, hsl, rgb, hex);
+}
+
+function hslToRGB(hsl) {
+  let h = hsl.h;
+  let s = hsl.s;
+  let l = hsl.l;
+  h = h;
+  s = s / 100;
+  l = l / 100;
+
+  let c = (1 - Math.abs(2 * l - 1)) * s,
+    x = c * (1 - Math.abs(((h / 60) % 2) - 1)),
+    m = l - c / 2,
+    r = 0,
+    g = 0,
+    b = 0;
+  if (0 <= h && h < 60) {
+    r = c;
+    g = x;
+    b = 0;
+  } else if (60 <= h && h < 120) {
+    r = x;
+    g = c;
+    b = 0;
+  } else if (120 <= h && h < 180) {
+    r = 0;
+    g = c;
+    b = x;
+  } else if (180 <= h && h < 240) {
+    r = 0;
+    g = x;
+    b = c;
+  } else if (240 <= h && h < 300) {
+    r = x;
+    g = 0;
+    b = c;
+  } else if (300 <= h && h < 360) {
+    r = c;
+    g = 0;
+    b = x;
+  }
+  r = Math.round((r + m) * 255);
+  g = Math.round((g + m) * 255);
+  b = Math.round((b + m) * 255);
+  return { r, g, b };
+}
+
+function rgbToHex(rgb) {
+  const r = rgb.r.toString(16).padStart(2, "0");
+  const g = rgb.g.toString(16).padStart(2, "0");
+  const b = rgb.b.toString(16).padStart(2, "0");
+
+  const hexColor = `#${r}${g}${b}`;
+  return hexColor;
+}
+
+function displayHarmony(calcHarmonyArray, hsl, rgb, hex) {
+  const colorBox = document.querySelector(".color-container:nth-child(" + (calcHarmonyArray.indexOf(hsl) + 1) + ") .color");
+  const hexText = document.querySelector(".color-container:nth-child(" + (calcHarmonyArray.indexOf(hsl) + 1) + ") p");
+  const rgbText = document.querySelector(".color-container:nth-child(" + (calcHarmonyArray.indexOf(hsl) + 1) + ") p+p");
+  const hslText = document.querySelector(".color-container:nth-child(" + (calcHarmonyArray.indexOf(hsl) + 1) + ") p+p+p");
+
+  colorBox.style.backgroundColor = `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
+  hexText.textContent = `hex: ${hex}`;
+  rgbText.textContent = `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`;
+  hslText.textContent = `hsl(${hsl.h}, ${hsl.s}%, ${hsl.l}%)`;
 }
